bc774568ef5f4b5e18c46959fd2fa737bfb674c7
BOUBRIT Nacim <nacim@swissdidata.com>
2022-09-30 22:19:05 +0200

    add ConnectionQueryFactory

---
+++ src/ConnectionQueryFactory.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace QueryJson;
+use Illuminate\Contracts\Container\Container;
+use QueryJson\ConnectionQueryJson\ConnectionQuery;
+
+class ConnectionQueryFactory
+{
+    /**
+     * @var Container
+     */
+    private $container;
+
+    public function __construct(Container $container)
+    {
+        $this->container = $container;
+    }
+
+    public function make(string $name): ConnectionQuery
+    {
+        $concrete = $this->getConcreteClassName($name);
+
+        $this->container->make($class);
+    }
+
+    /**
+     * @param string $name
+     * @return string
+     */
+    private function getConcreteClassName(string $name): string
+    {
+        $abstract = ConnectionQuery::class;
+
+        $abstractBaseName = class_basename($abstract);
+
+        $concreteClassName = ucfirst($name) . $abstractBaseName;
+
+        return str_replace($abstractBaseName, $concreteClassName, $abstract);
+    }
+}
