07d0eb14c1a77074e0bd4e3a569fb0b88e527e5e
BOUBRIT Nacim <nacim@swissdidata.com>
2022-10-05 23:00:26 +0200

    use jsonQueryBuilder in query manager

--- src/QueryJsonManager.php
+++ src/QueryJsonManager.php
@@ -3,9 +3,6 @@
 namespace QueryJson;
 
 use \Illuminate\Database\ConnectionInterface as Connection;
-use QueryJson\Connections\Connection as ConnectionsConnection;
-use QueryJson\Connections\ConnectionFactory;
-
 class QueryJsonManager
 {
     /**
@@ -18,14 +15,19 @@ class QueryJsonManager
      */
     private $driver;
 
-    private $connectionFactory;
+    /**
+     * @var JsonQueryBuilder
+     */
+    private $JsonQueryBuilder;
 
-    public function __construct(Connection $connection, ConnectionFactory $connectionFactory)
+    public function __construct(Connection $connection, JsonQueryBuilder $JsonQueryBuilder)
     {
         $this->connection = $connection;
         $this->query =  $connection->query();
         $this->driver = $connection->getDriverName();
-        $this->connectionFactory = $connectionFactory;
+        $this->JsonQueryBuilder = $JsonQueryBuilder;
+
+        $this->JsonQueryBuilder->setDriver($this->driver);
     }
 
     /**
@@ -33,18 +35,12 @@ class QueryJsonManager
      */
     public function extendQueryBuilder(): void
     {
-        $connectionQuery = $this->getConnectionQuery();
+        foreach (get_class_methods($this->JsonQueryBuilder) as $name) {
+            if (in_array(strtolower($name), ['setdriver', '__construct'], true)) {
+                continue;
+            }
 
-        foreach (get_class_methods($connectionQuery) as $name) {
-            $this->query->macro($name, $connectionQuery->{$name}());
+            $this->query->macro($name, $this->JsonQueryBuilder->{$name}());
         }
-    }
-
-    /**
-     * @return ConnectionsConnection
-     */
-    private function getConnectionQuery(): ConnectionsConnection
-    {
-        return $this->connectionFactory->make($this->driver);
     }
 }
