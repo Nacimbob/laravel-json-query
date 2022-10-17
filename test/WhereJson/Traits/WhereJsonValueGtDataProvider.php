9e4c6437e17ada809994536451afb3d3563ea656
BOUBRIT Nacim <nacim@swissdidata.com>
2022-10-17 21:38:06 +0200

    add test case for greater then json value

--- test/WhereJson/Traits/WhereJsonValueGtDataProvider.php
+++ test/WhereJson/Traits/WhereJsonValueGtDataProvider.php
@@ -58,6 +58,49 @@ trait WhereJsonValueGtDataProvider
                 10,
                 2
             ],
+            [
+                [
+                    json_encode([
+                        "key_02" => "-10",
+                        "another_key" => -768
+                    ]),
+                    json_encode([
+                        "key_02" => -768
+                    ]),
+                    json_encode([
+                        "key_02" => "10",
+                        "another_key" => 4937
+                    ]),
+                    json_encode([
+                        "key_02" => "21",
+                    ]),
+                    json_encode([
+                        "key_02" => 42,
+                    ]),
+                    json_encode([
+                        "name_02" => 42,
+                    ]),
+                    json_encode([
+                        "key_05" => "11",
+                        "another_key" => 20
+                    ]),
+                    json_encode([
+                        "key_03" => "22",
+                        "another_key" => 0
+                    ]),
+                    json_encode([
+                        "key_01" => -10,
+                        "another_key" => -35
+                    ]),
+                    json_encode([
+                        "name" => -768
+                    ])
+                ],
+                "->key_02",
+                '>=',
+                10,
+                3
+            ],
         ];
     }
 }
